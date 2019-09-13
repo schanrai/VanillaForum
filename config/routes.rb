b4f567748184f9c194e7c2f588b19cfea08e9319
Sushi Chanrai <sushi@pathworks.co>
2019-09-13 14:49:08 -0400

    configure migrations install gems

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :upvotes
+  resources :posts
+  resources :topics
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
