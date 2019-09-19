6d31c6c977edbed2aeda8ab1d9c635a97aa3c8bd
Sushi Chanrai <sushi@pathworks.co>
2019-09-19 14:36:06 -0400

    develop basic upvote create action

--- app/controllers/upvotes_controller.rb
+++ app/controllers/upvotes_controller.rb
@@ -1,14 +1,14 @@
 class UpvotesController < ApplicationController
 
   def create
-    params[:upvote][:user_id] = session[:user_i
+    params[:upvote][:user_id] = session[:user_id]
     @user = User.find(session[:user_id])
     #store user in @user for show page where redirected to
     #creates a ride with user_id and attraction_id attributes (always built off attraction?)
     @upvote = Upvote.new(upvote_params)
     #runs the take_ride method on the associated objects
     if @upvote.save
-      flash[:notice] = @ride.take_ride
+      flash[:error] = "Post sucessfully upvoted"
       redirect_to user_path(@user)
     end
   end
@@ -20,6 +20,5 @@ class UpvotesController < ApplicationController
     def upvote_params
       params.require(:upvote).permit(:user_id,:post_id)
     end
-  end
 
 end
