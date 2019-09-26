7ee340a53027b724f6a88136007abd42887bfd2a
Sushi Chanrai <sushi@pathworks.co>
2019-09-26 16:50:36 -0400

    refactor not_upvoted_by_current_user method

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -2,8 +2,7 @@ module TopicsHelper
 
 
   def not_upvoted_by_current_user?(post)
-    a = post.upvotes.select { |upvote| upvote.user_id == current_user[:id] }
-    a.empty?
+    post.upvotes.select { |upvote| upvote.user_id == current_user[:id] }.empty?
   end
 
 end
