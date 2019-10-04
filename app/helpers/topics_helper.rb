43d5bf0ff9df35f8d3811c0f142225126223bbbf
Sushi Chanrai <sushi@pathworks.co>
2019-10-04 09:55:31 -0400

    fixed not upvoted method

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -3,9 +3,9 @@ module TopicsHelper
 
   def not_upvoted_by_current_user?(post)
     #post.upvotes.select { |upvote| upvote.user_id == current_user[:id] }.empty?
-    post.upvotes.where("user_id = ? ", current_user[:id])
+    post.upvotes.where("user_id = ? ", current_user[:id]).empty?
   end
 
-  
+
 
 end
